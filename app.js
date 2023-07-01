035697e5dda585ebc6c402d5b22e224c0d2937a1
Oksana Bykova <ishkulova.oksana1@yandex.ru>
2023-07-01 11:05:47 +0300

    готов логгер и изменения в app для .api

--- app.js
+++ app.js
@@ -14,11 +14,15 @@ const auth = require('./middlwares/auth');
 const { RegURL } = require('./utils/constants');
 const { login, createUser } = require('./controllers/users');
 
+const { requestLogger, errorLogger } = require('./middlwares/logger');
+
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
 app.use(express.json());
+
+app.use(requestLogger);
 
 app.post(
   '/signup',
@@ -51,7 +55,10 @@ app.get('/signout', (req, res) => {
   res.clearCookie('jwt').send({ message: 'Выход' });
 });
 
-app.use(router);
+//app.use(router);
+app.use('/api', require('../router'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
